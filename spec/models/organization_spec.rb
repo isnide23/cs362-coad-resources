ce1710422a755569b3ba4c81f45e565884ea87a8
Ian Snyder <isnide23@gmail.com>
2021-02-07 10:31:08 -0800

    regions and organizations started

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
 
+    it 'exists' do
+        Organization.new
+    end
+
+    
+
+
+
 end
--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -11,6 +11,22 @@ RSpec.describe Region, type: :model do
     expect(region).to respond_to(:name)
   end
 
+  it 'is expected to have a region of unspecified' do
+    expect(Region.unspecified.name).to eq('Unspecified')
+  end
+
+  it 'to_s to returns a name' do
+    name = 'exName'
+    region = Region.new(name: name)
+    expect(region.to_s).to eq(name)
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name) }
+    it { should validate_uniqueness_of(:name).case_insensitive }
+  end
+
 
 end
 
