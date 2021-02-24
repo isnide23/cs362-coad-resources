ff8799911fdcace7d2c8cf63c4059412d85f498f
Ian Snyder <isnide23@gmail.com>
2021-02-23 16:43:33 -0800

    add has many ticket test to region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe Region, type: :model do
     expect(region).to respond_to(:name)
   end
 
+  it 'has many tickets' do
+    region = Region.new
+    expect(region).to respond_to(:tickets)
+  end
+
   it 'is expected to have a region of unspecified' do
     expect(Region.unspecified.name).to eq('Unspecified')
   end
@@ -19,6 +24,7 @@ RSpec.describe Region, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    #eleaborate on validation tests
     it { should validate_length_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
